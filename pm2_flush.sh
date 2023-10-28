7d58ff453d78a69919e08c8e6931f081cb93ffd1
Anderson S Souza <pfanderson3@gmail.com>
2023-10-28 12:38:50 -0400

    first commit

---
+++ pm2_flush.sh
@@ -0,0 +1,66 @@
+Last login: Sat Oct 28 11:25:05 on ttys003
+(base) andersonsouza@MacBook-Air-de-Anderson auto-shell-pm2-clear-logs % ls
+(base) andersonsouza@MacBook-Air-de-Anderson auto-shell-pm2-clear-logs % nano 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  UW PICO 5.09                                          New Buffer                                           Modified  
+
+#!/bin /bash
+# this script flushes pm2 once a day
+
+# run the pm2 flush command
+pm2 flush
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                [ Unknown Command: ^S ]                                                
+^G Get Help        ^O WriteOut        ^R Read File       ^Y Prev Pg         ^K Cut Text        ^C Cur Pos         
+^X Exit            ^J Justify         ^W Where is        ^V Next Pg         ^U UnCut Text      ^T To Spell        
